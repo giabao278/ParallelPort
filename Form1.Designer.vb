07bfaac958b8d68b413d9dbf291e5b8cafaef240
Duong Gia Bao <38368251+DGBvn@users.noreply.github.com>
2021-08-30 17:18:59 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Led0_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.Led1_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.Led3_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.Led2_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.Led7_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.Led6_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.Led5_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.Led4_btn = New Guna.UI2.WinForms.Guna2CircleButton()
+        Me.dec_textbox = New System.Windows.Forms.TextBox()
+        Me.hex_textbox = New System.Windows.Forms.TextBox()
+        Me.oct_textbox = New System.Windows.Forms.TextBox()
+        Me.bin_textbox = New System.Windows.Forms.TextBox()
+        Me.hex_lable = New System.Windows.Forms.Label()
+        Me.dec_lable = New System.Windows.Forms.Label()
+        Me.bin_lable = New System.Windows.Forms.Label()
+        Me.oct_lable = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Led0_btn
+        '
+        Me.Led0_btn.CheckedState.Parent = Me.Led0_btn
+        Me.Led0_btn.CustomImages.Parent = Me.Led0_btn
+        Me.Led0_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led0_btn.ForeColor = System.Drawing.Color.White
+        Me.Led0_btn.HoverState.Parent = Me.Led0_btn
+        Me.Led0_btn.Location = New System.Drawing.Point(422, 460)
+        Me.Led0_btn.Name = "Led0_btn"
+        Me.Led0_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led0_btn.ShadowDecoration.Parent = Me.Led0_btn
+        Me.Led0_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led0_btn.TabIndex = 0
+        '
+        'Led1_btn
+        '
+        Me.Led1_btn.CheckedState.Parent = Me.Led1_btn
+        Me.Led1_btn.CustomImages.Parent = Me.Led1_btn
+        Me.Led1_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led1_btn.ForeColor = System.Drawing.Color.White
+        Me.Led1_btn.HoverState.Parent = Me.Led1_btn
+        Me.Led1_btn.Location = New System.Drawing.Point(376, 460)
+        Me.Led1_btn.Name = "Led1_btn"
+        Me.Led1_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led1_btn.ShadowDecoration.Parent = Me.Led1_btn
+        Me.Led1_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led1_btn.TabIndex = 1
+        '
+        'Led3_btn
+        '
+        Me.Led3_btn.CheckedState.Parent = Me.Led3_btn
+        Me.Led3_btn.CustomImages.Parent = Me.Led3_btn
+        Me.Led3_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led3_btn.ForeColor = System.Drawing.Color.White
+        Me.Led3_btn.HoverState.Parent = Me.Led3_btn
+        Me.Led3_btn.Location = New System.Drawing.Point(284, 460)
+        Me.Led3_btn.Name = "Led3_btn"
+        Me.Led3_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led3_btn.ShadowDecoration.Parent = Me.Led3_btn
+        Me.Led3_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led3_btn.TabIndex = 3
+        '
+        'Led2_btn
+        '
+        Me.Led2_btn.CheckedState.Parent = Me.Led2_btn
+        Me.Led2_btn.CustomImages.Parent = Me.Led2_btn
+        Me.Led2_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led2_btn.ForeColor = System.Drawing.Color.White
+        Me.Led2_btn.HoverState.Parent = Me.Led2_btn
+        Me.Led2_btn.Location = New System.Drawing.Point(330, 460)
+        Me.Led2_btn.Name = "Led2_btn"
+        Me.Led2_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led2_btn.ShadowDecoration.Parent = Me.Led2_btn
+        Me.Led2_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led2_btn.TabIndex = 2
+        '
+        'Led7_btn
+        '
+        Me.Led7_btn.CheckedState.Parent = Me.Led7_btn
+        Me.Led7_btn.CustomImages.Parent = Me.Led7_btn
+        Me.Led7_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led7_btn.ForeColor = System.Drawing.Color.White
+        Me.Led7_btn.HoverState.Parent = Me.Led7_btn
+        Me.Led7_btn.Location = New System.Drawing.Point(100, 460)
+        Me.Led7_btn.Name = "Led7_btn"
+        Me.Led7_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led7_btn.ShadowDecoration.Parent = Me.Led7_btn
+        Me.Led7_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led7_btn.TabIndex = 7
+        '
+        'Led6_btn
+        '
+        Me.Led6_btn.CheckedState.Parent = Me.Led6_btn
+        Me.Led6_btn.CustomImages.Parent = Me.Led6_btn
+        Me.Led6_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led6_btn.ForeColor = System.Drawing.Color.White
+        Me.Led6_btn.HoverState.Parent = Me.Led6_btn
+        Me.Led6_btn.Location = New System.Drawing.Point(146, 460)
+        Me.Led6_btn.Name = "Led6_btn"
+        Me.Led6_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led6_btn.ShadowDecoration.Parent = Me.Led6_btn
+        Me.Led6_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led6_btn.TabIndex = 6
+        '
+        'Led5_btn
+        '
+        Me.Led5_btn.CheckedState.Parent = Me.Led5_btn
+        Me.Led5_btn.CustomImages.Parent = Me.Led5_btn
+        Me.Led5_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led5_btn.ForeColor = System.Drawing.Color.White
+        Me.Led5_btn.HoverState.Parent = Me.Led5_btn
+        Me.Led5_btn.Location = New System.Drawing.Point(192, 460)
+        Me.Led5_btn.Name = "Led5_btn"
+        Me.Led5_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led5_btn.ShadowDecoration.Parent = Me.Led5_btn
+        Me.Led5_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led5_btn.TabIndex = 5
+        '
+        'Led4_btn
+        '
+        Me.Led4_btn.CheckedState.Parent = Me.Led4_btn
+        Me.Led4_btn.CustomImages.Parent = Me.Led4_btn
+        Me.Led4_btn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Led4_btn.ForeColor = System.Drawing.Color.White
+        Me.Led4_btn.HoverState.Parent = Me.Led4_btn
+        Me.Led4_btn.Location = New System.Drawing.Point(238, 460)
+        Me.Led4_btn.Name = "Led4_btn"
+        Me.Led4_btn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.Led4_btn.ShadowDecoration.Parent = Me.Led4_btn
+        Me.Led4_btn.Size = New System.Drawing.Size(40, 40)
+        Me.Led4_btn.TabIndex = 4
+        '
+        'dec_textbox
+        '
+        Me.dec_textbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dec_textbox.Location = New System.Drawing.Point(178, 197)
+        Me.dec_textbox.Name = "dec_textbox"
+        Me.dec_textbox.Size = New System.Drawing.Size(130, 30)
+        Me.dec_textbox.TabIndex = 9
+        '
+        'hex_textbox
+        '
+        Me.hex_textbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hex_textbox.Location = New System.Drawing.Point(178, 233)
+        Me.hex_textbox.Name = "hex_textbox"
+        Me.hex_textbox.Size = New System.Drawing.Size(130, 30)
+        Me.hex_textbox.TabIndex = 10
+        '
+        'oct_textbox
+        '
+        Me.oct_textbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.oct_textbox.Location = New System.Drawing.Point(178, 269)
+        Me.oct_textbox.Name = "oct_textbox"
+        Me.oct_textbox.Size = New System.Drawing.Size(130, 30)
+        Me.oct_textbox.TabIndex = 11
+        '
+        'bin_textbox
+        '
+        Me.bin_textbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bin_textbox.Location = New System.Drawing.Point(178, 305)
+        Me.bin_textbox.Name = "bin_textbox"
+        Me.bin_textbox.Size = New System.Drawing.Size(130, 30)
+        Me.bin_textbox.TabIndex = 12
+        '
+        'hex_lable
+        '
+        Me.hex_lable.AutoSize = True
+        Me.hex_lable.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hex_lable.Location = New System.Drawing.Point(80, 236)
+        Me.hex_lable.Name = "hex_lable"
+        Me.hex_lable.Size = New System.Drawing.Size(56, 25)
+        Me.hex_lable.TabIndex = 13
+        Me.hex_lable.Text = "HEX"
+        '
+        'dec_lable
+        '
+        Me.dec_lable.AutoSize = True
+        Me.dec_lable.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dec_lable.Location = New System.Drawing.Point(80, 200)
+        Me.dec_lable.Name = "dec_lable"
+        Me.dec_lable.Size = New System.Drawing.Size(57, 25)
+        Me.dec_lable.TabIndex = 14
+        Me.dec_lable.Text = "DEC"
+        '
+        'bin_lable
+        '
+        Me.bin_lable.AutoSize = True
+        Me.bin_lable.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bin_lable.Location = New System.Drawing.Point(80, 308)
+        Me.bin_lable.Name = "bin_lable"
+        Me.bin_lable.Size = New System.Drawing.Size(47, 25)
+        Me.bin_lable.TabIndex = 15
+        Me.bin_lable.Text = "BIN"
+        '
+        'oct_lable
+        '
+        Me.oct_lable.AutoSize = True
+        Me.oct_lable.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.oct_lable.Location = New System.Drawing.Point(80, 272)
+        Me.oct_lable.Name = "oct_lable"
+        Me.oct_lable.Size = New System.Drawing.Size(59, 25)
+        Me.oct_lable.TabIndex = 16
+        Me.oct_lable.Text = "OCT"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(882, 553)
+        Me.Controls.Add(Me.oct_lable)
+        Me.Controls.Add(Me.bin_lable)
+        Me.Controls.Add(Me.dec_lable)
+        Me.Controls.Add(Me.hex_lable)
+        Me.Controls.Add(Me.bin_textbox)
+        Me.Controls.Add(Me.oct_textbox)
+        Me.Controls.Add(Me.hex_textbox)
+        Me.Controls.Add(Me.dec_textbox)
+        Me.Controls.Add(Me.Led7_btn)
+        Me.Controls.Add(Me.Led6_btn)
+        Me.Controls.Add(Me.Led5_btn)
+        Me.Controls.Add(Me.Led4_btn)
+        Me.Controls.Add(Me.Led3_btn)
+        Me.Controls.Add(Me.Led2_btn)
+        Me.Controls.Add(Me.Led1_btn)
+        Me.Controls.Add(Me.Led0_btn)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Test Parallel Port"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Led0_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents Led1_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents Led3_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents Led2_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents Led7_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents Led6_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents Led5_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents Led4_btn As Guna.UI2.WinForms.Guna2CircleButton
+    Friend WithEvents dec_textbox As TextBox
+    Friend WithEvents hex_textbox As TextBox
+    Friend WithEvents oct_textbox As TextBox
+    Friend WithEvents bin_textbox As TextBox
+    Friend WithEvents hex_lable As Label
+    Friend WithEvents dec_lable As Label
+    Friend WithEvents bin_lable As Label
+    Friend WithEvents oct_lable As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,160 @@
+﻿Public Class Form1
+    Private Sub dec_textbox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles dec_textbox.KeyPress
+        If e.KeyChar = Chr(Keys.Enter) Then
+            Dim dec_txtb As Integer = Val(dec_textbox.Text)
+
+            hex_textbox.Text = UCase(Convert.ToString(dec_txtb, 16))
+            oct_textbox.Text = Convert.ToString(dec_txtb, 8)
+
+            Dim bin_txtb As String = Convert.ToString(dec_txtb, 2)
+
+            If Len(bin_txtb) < 8 Then
+                Dim lgt As Integer = 8 - Len(bin_txtb)
+                Do Until lgt >= 8
+                    bin_txtb = Convert.ToString(0) & bin_txtb
+                    lgt = Len(bin_txtb)
+                Loop
+            End If
+            bin_textbox.Text = bin_txtb
+
+            'check led function
+            For cnt = 1 To 8
+                checkLed((cnt - 1), Mid(bin_txtb, 9 - cnt, 1))
+            Next
+        End If
+    End Sub
+
+    Private Sub hex_textbox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles hex_textbox.KeyPress
+        If e.KeyChar = Chr(Keys.Enter) Then
+            Dim hex_txtb As String = hex_textbox.Text
+            Dim hex_convert As Integer = Convert.ToInt16(hex_txtb, 16)
+
+            dec_textbox.Text = Convert.ToString(hex_convert, 10)
+            oct_textbox.Text = Convert.ToString(hex_convert, 8)
+            hex_textbox.Text = UCase(hex_textbox.Text)
+
+            Dim bin_txtb As String = Convert.ToString(hex_convert, 2)
+
+            If Len(bin_txtb) < 8 Then
+                Dim lgt As Integer = 8 - Len(bin_txtb)
+                Do Until lgt >= 8
+                    bin_txtb = Convert.ToString(0) & bin_txtb
+                    lgt = Len(bin_txtb)
+                Loop
+            End If
+            bin_textbox.Text = bin_txtb
+
+            'check led function
+            For cnt = 1 To 8
+                checkLed((cnt - 1), Mid(bin_txtb, 9 - cnt, 1))
+            Next
+
+        End If
+    End Sub
+
+    Private Sub oct_textbox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles oct_textbox.KeyPress
+        If e.KeyChar = Chr(Keys.Enter) Then
+            Dim oct_txtb As String = oct_textbox.Text
+            Dim oct_convert As Integer = Convert.ToInt16(oct_txtb, 8)
+
+            dec_textbox.Text = Convert.ToString(oct_convert, 10)
+            hex_textbox.Text = UCase(Convert.ToString(oct_convert, 16))
+
+            Dim bin_txtb As String = Convert.ToString(oct_convert, 2)
+
+            If Len(bin_txtb) < 8 Then
+                Dim lgt As Integer = 8 - Len(bin_txtb)
+                Do Until lgt >= 8
+                    bin_txtb = Convert.ToString(0) & bin_txtb
+                    lgt = Len(bin_txtb)
+                Loop
+            End If
+            bin_textbox.Text = bin_txtb
+
+            'check led function
+            For cnt = 1 To 8
+                checkLed((cnt - 1), Mid(bin_txtb, 9 - cnt, 1))
+            Next
+        End If
+    End Sub
+
+    Private Sub bin_textbox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles bin_textbox.KeyPress
+        If e.KeyChar = Chr(Keys.Enter) Then
+            Dim bin_txtb As Integer = Val(bin_textbox.Text)
+            Dim bin_convert As Integer = Convert.ToInt16(bin_txtb, 2)
+
+            dec_textbox.Text = Convert.ToString(bin_convert, 10)
+            hex_textbox.Text = UCase(Convert.ToString(bin_convert, 16))
+            oct_textbox.Text = Convert.ToString(bin_convert, 8)
+
+            Dim bin_cmp As String = Convert.ToString(bin_convert, 2)
+
+            If Len(bin_cmp) < 8 Then
+                Dim lgt As Integer = 8 - Len(bin_cmp)
+                Do Until lgt >= 8
+                    bin_cmp = Convert.ToString(0) & bin_cmp
+                    lgt = Len(bin_cmp)
+                Loop
+            End If
+            bin_textbox.Text = Convert.ToString(bin_cmp)
+
+            'check led function
+            For cnt = 1 To 8
+                checkLed((cnt - 1), Mid(bin_cmp, 9 - cnt, 1))
+            Next
+        End If
+    End Sub
+
+    Public Sub checkLed(numOfLed As Integer, logicVal As Integer)
+        Select Case numOfLed
+            Case 0
+                If logicVal = 1 Then
+                    Led0_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led0_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+            Case 1
+                If logicVal = 1 Then
+                    Led1_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led1_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+            Case 2
+                If logicVal = 1 Then
+                    Led2_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led2_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+            Case 3
+                If logicVal = 1 Then
+                    Led3_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led3_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+            Case 4
+                If logicVal = 1 Then
+                    Led4_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led4_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+            Case 5
+                If logicVal = 1 Then
+                    Led5_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led5_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+            Case 6
+                If logicVal = 1 Then
+                    Led6_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led6_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+            Case 7
+                If logicVal = 1 Then
+                    Led7_btn.FillColor = Color.Red
+                ElseIf logicVal = 0 Then
+                    Led7_btn.FillColor = Color.FromArgb(94, 148, 255)
+                End If
+        End Select
+    End Sub
+End Class
